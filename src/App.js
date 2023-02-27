e61338081e75e8254315d195145339d26fd0fee4
hardik3rddigital <hardik.s@3rddigital.com>
2023-02-27 16:06:18 +0530

    Material UI Container

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React from 'react'
-import FirstFile from './Components/FirstFile'
-import MaterialUIButton from './Components/MaterialUIButton'
-import MaterialUIButtonGroup from './Components/MaterialUIButtonGroup'
-import MaterialUICheckbox from './Components/MaterialUICheckbox'
-import MaterialUIRadio from './Components/MaterialUIRadio'
-import MaterialUISlider from './Components/MaterialUISlider'
-import MaterialUIRangeSlider from './Components/MaterialUIRangeSlider'
-import MaterialUISelect from './Components/MaterialUISelect'
-import MaterialUIInput from './Components/MaterialUIInput'
-import MaterialUISwitch from './Components/MaterialUISwitch'
-import MaterialUIBox from './Components/MaterialUIBox'
+import React from "react";
+import FirstFile from "./Components/FirstFile";
+import MaterialUIButton from "./Components/MaterialUIButton";
+import MaterialUIButtonGroup from "./Components/MaterialUIButtonGroup";
+import MaterialUICheckbox from "./Components/MaterialUICheckbox";
+import MaterialUIRadio from "./Components/MaterialUIRadio";
+import MaterialUISlider from "./Components/MaterialUISlider";
+import MaterialUIRangeSlider from "./Components/MaterialUIRangeSlider";
+import MaterialUISelect from "./Components/MaterialUISelect";
+import MaterialUIInput from "./Components/MaterialUIInput";
+import MaterialUISwitch from "./Components/MaterialUISwitch";
+import MaterialUIBox from "./Components/MaterialUIBox";
+import MaterialUIContainer from "./Components/MaterialUIContainer";
+import { Container } from "@mui/material";
 
 function App() {
   return (
     <>
-    <FirstFile />
-    <MaterialUIButton />
-    <MaterialUIButtonGroup />
-    <MaterialUICheckbox />
-    <MaterialUIRadio />
-    <MaterialUISlider />
-    <MaterialUIRangeSlider />
-    <MaterialUISelect />
-    <MaterialUIInput />
-    <MaterialUISwitch />
-    <MaterialUIBox />
-
+      <Container>
+        <FirstFile />
+        <MaterialUIButton />
+        <MaterialUIButtonGroup />
+        <MaterialUICheckbox />
+        <MaterialUIRadio />
+        <MaterialUISlider />
+        <MaterialUIRangeSlider />
+        <MaterialUISelect />
+        <MaterialUIInput />
+        <MaterialUISwitch />
+        <MaterialUIBox />
+        <MaterialUIContainer />
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/MaterialUIContainer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+function MaterialUIContainer() {
+  return (
+    <>
+      <h2>MaterialUI - Container</h2>
+      <h4>MaterialUI - Basic Container</h4>
+      <Container maxWidth="xl">
+        <p>Content Inside the Container</p>
+      </Container>
+    </>
+  );
+}
+
+export default MaterialUIContainer;
